7c9ec73d35e59ab2af5a4c2d5210ad8c8a8d99a0
KaziShahHamza <kazishahhamza@outlook.com>
2025-08-14 15:52:46 +0600

    Setup project folder structure

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,11 +1,9 @@
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-      <h1>Home</h1>
-      <Button>Button</Button>
+      <h1>HomePage</h1>
     </>
   );
 }
